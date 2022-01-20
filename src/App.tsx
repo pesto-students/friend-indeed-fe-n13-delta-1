a42f31844c3a9dd5e9c532bbc71e538f6ff2de0f
Joel Vinay Kumar <lavotivinay@gmail.com>
2022-01-20 11:46:27 +0530

    Change page title

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            React
+            FriendIndeed
           </a>
           <span>, </span>
           <a
